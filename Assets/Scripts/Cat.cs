12af60992eea256da1a625c9673d523c1e8b2cd3
gil-cl <gilclaeyssens@hotmail.com>
2021-08-05 18:28:11 +0200

    Done

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : Animal
+{
+    private string name;
+    private float eatSpeed = 1.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Eat()
+    {
+        base.Eat();
+    }
+
+    public override void MakeNoice()
+    {
+        // Miauw. 
+        base.MakeNoice();
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name; 
+        }
+        set
+        {
+            if (name.Length > 10)
+            {
+                Debug.Log("Naam te lang"); 
+            }
+            else
+            {
+                name = value; 
+            }
+        }
+    }
+}
